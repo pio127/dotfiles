0309bcf055f0c7a8922f941709cad5fd98a23ac7
Piotr <pio.andr@gmail.com>
2022-04-20 13:52:31 +0200

    Fix gathering script

--- gather_current.sh
+++ gather_current.sh
@@ -3,9 +3,9 @@
 cp ~/.vimrc                .
 cp ~/.zshrc                .
 cp ~/.tmux.conf            .
-cp ~/.config/mc/ini        .
-cp ~/.config/mc/mc.keymap  .
-cp ~/.config/mc/panels.ini .
+cp ~/.config/mc/ini        ./.config/mc/
+cp ~/.config/mc/mc.keymap  ./.config/mc/
+cp ~/.config/mc/panels.ini ./.config/mc/
 
 echo "Files updated:"
 git status --short
