af96c3947324ca9544246acb452bb3c129d19199
Piotr <pio.andr@gmail.com>
2022-05-07 02:19:48 +0200

    Remove mc ini and panels.ini updating

--- gather_current.sh
+++ gather_current.sh
@@ -3,9 +3,7 @@
 cp ~/.vimrc                .
 cp ~/.zshrc                .
 cp ~/.tmux.conf            .
-cp ~/.config/mc/ini        ./.config/mc/
 cp ~/.config/mc/mc.keymap  ./.config/mc/
-cp ~/.config/mc/panels.ini ./.config/mc/
 
 echo "Files updated:"
 git status --short
