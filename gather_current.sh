57f02be49052342262931c4bc8f20dbdb33f0120
Piotr <Piotr>
2022-05-17 13:30:53 +0200

    Add alacritty config

--- gather_current.sh
+++ gather_current.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cp ~/.vimrc                .
-cp ~/.zshrc                .
-cp ~/.tmux.conf            .
-cp ~/.config/mc/mc.keymap  ./.config/mc/
+cp ~/.vimrc                          .
+cp ~/.zshrc                          .
+cp ~/.tmux.conf                      .
+cp ~/.config/mc/mc.keymap            ./.config/mc/
+cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/
 
 echo "Files updated:"
 git status --short
