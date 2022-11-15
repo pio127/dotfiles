4a321a606ee629980f17bedc3789d229b0aa090e
Piotr <Piotr>
2022-11-15 20:13:01 +0100

    Update current configs gathering after i3 add

--- gather_current.sh
+++ gather_current.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cp ~/.vimrc                          .
-cp ~/.zshrc                          .
-cp ~/.tmux.conf                      .
+cp ~/.vimrc                          ./
+cp ~/.zshrc                          ./
+cp ~/.tmux.conf                      ./
 cp ~/.config/nvim/init.vim           ./.config/nvim/
 cp ~/.config/mc/mc.keymap            ./.config/mc/
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/
+cp ~/.config/i3/config               ./.config/i3/
 
 echo "Files updated:"
 git status --short
