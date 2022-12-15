866d459296564b76ed46b3b3814524b98cf321f0
Piotr <pio.andr@gmail.com>
2022-12-15 22:17:10 +0100

    Change init.vim to init.lua

--- gather_current.sh
+++ gather_current.sh
@@ -3,7 +3,7 @@
 cp ~/.vimrc                          ./
 cp ~/.zshrc                          ./
 cp ~/.tmux.conf                      ./
-cp ~/.config/nvim/init.vim           ./.config/nvim/
+cp ~/.config/nvim/init.lua           ./.config/nvim/
 cp ~/.config/mc/mc.keymap            ./.config/mc/
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/
 cp ~/.config/i3/config               ./.config/i3/
