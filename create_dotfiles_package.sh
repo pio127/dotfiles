40ce81b77a395e3c7e06cddc0f622e85a276d851
Piotr <pio.andr@gmail.com>
2022-12-27 21:55:26 +0100

    Add dotfiles package creator idea

---
+++ create_dotfiles_package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Prepares a tar.gz package with all dotfiles and downloaded plugins.
+# Steps:
+#     1. Downloads or builds all required tools and copies them to a bin directory.
+#     2. Clones all plugins from github.
