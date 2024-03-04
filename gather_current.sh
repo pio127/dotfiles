08540b3d5643784d3e05ae681ae8328a8f33b5e8
Piotr <pio.andr@gmail.com>
2024-03-05 00:01:23 +0100

    Remove i3 config for DE/WM independent workflow

--- gather_current.sh
+++ gather_current.sh
@@ -6,7 +6,6 @@ cp ~/.tmux.conf                      ./
 cp ~/.config/nvim/init.lua           ./.config/nvim/
 cp ~/.config/mc/mc.keymap            ./.config/mc/
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/
-cp ~/.config/i3/config               ./.config/i3/
 
 echo "Files updated:"
 git status --short
