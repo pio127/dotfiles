1c1e769cdfc32cc8cf87d6b54897990fd90058cb
Piotr <pio.andr@gmail.com>
2024-03-10 13:17:48 +0100

    Simplify dotfiles gather scripts

--- gather_current.sh
+++ gather_current.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-cp ~/.vimrc                          ./
-cp ~/.zshrc                          ./
-cp ~/.tmux.conf                      ./
-cp ~/.config/nvim/init.lua           ./.config/nvim/
-cp ~/.config/mc/mc.keymap            ./.config/mc/
-cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/
-
-echo "Files updated:"
-git status --short
+echo -e "Dotfiles copied from home directory:"
+cp -v ~/.{zshrc,vimrc,tmux.conf} $(realpath .)
+echo -e "\nDotfiles updated:"
+git status --short .{zshrc,vimrc,tmux.conf}
